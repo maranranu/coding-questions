aa57106c65b2ee17a2e04ca1430effe657842ece
Kamnee Maran <kamneemaran45@gmail.com>
2021-03-06 19:52:38 +0530

    add problem statement

--- fraction.py
+++ fraction.py
@@ -1,14 +1,16 @@
+"""
+Given a positive integer N, the task is to find the Nth natural number after removing all the natural numbers containing digit 9.
+"""
+
 class Solution:
     def findNth(self,N):
         result = 0
         base = 1
 
         while N > 0:
-            print('num: ', N)
             result += base*(N % 9)
             N = N // 9
             base = base * 10
-            print(result)
         return result
 
             
