c444ab893141f6cfc2f8bb2b42625929c5cf2ba3
Kamnee Maran <kamneemaran45@gmail.com>
2021-03-06 19:49:42 +0530

    add problem statement

--- covid_spread.py
+++ covid_spread.py
@@ -1,3 +1,13 @@
+"""
+Aterp is the head nurse at city hospital. City hospital contains R*C number of wards and structure of hospital is in the form of a 2-D matrix.
+Given a matrix of dimension R*C where each cell in the matrix can have values 0, 1 or 2 which has the following meaning:
+0 : Empty ward
+1 : Cells have uninfected patients
+2 : Cells have infected patients
+
+An infected patient at ward [i,j] can infect other uninfected patient at indexes [i-1,j], [i+1,j], [i,j-1], [i,j+1] (up, down, left and right) in unit time. Help Aterp determine the minimum units of time after which there won't remain any uninfected patient i.e all patients would be infected. If all patients are not infected after infinite units of time then simply return -1.
+
+"""
 from collections import deque
 
 class Solution:
